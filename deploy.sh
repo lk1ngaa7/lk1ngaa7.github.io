0478d9415b4e473a5c44e31f9905039fb2e0ad7a
liukai19 <liukai19@baidu.com>
2018-02-25 18:18:55 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@
 # Created Time: Sun Feb 25 18:16:01 2018
 #########################################################################
 #!/bin/bash
+cp -rf public/*  deploy/lk1ngaa7.github.io
 cd deploy/lk1ngaa7.github.io
 git add .
 git commit -m "update"
