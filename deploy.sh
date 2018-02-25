678389ffb28c78e5affa4d0bfbfc73eb279b1793
liukai19 <liukai19@baidu.com>
2018-02-25 18:17:55 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#########################################################################
+# File Name: deply.sh
+# Author: 
+# mail: 
+# Created Time: Sun Feb 25 18:16:01 2018
+#########################################################################
+#!/bin/bash
+cd deploy/lk1ngaa7.github.io
+git add .
+git commit -m "update"
+git push origin master
